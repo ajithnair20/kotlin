231a4998d36cd2f3d3b2e2833f4d7fe728633279
Alexander Udalov <alexander.udalov@jetbrains.com>
2020-11-13 19:54:30 +0100

    JVM IR: copy corresponding property+annotations for DefaultImpls methods
    Instead of using methodSignatureMapper which was an obsolete hack to
    make accessor names "getFoo" instead of "<get-foo>".
    
    In particular, this keeps the `@Deprecated` annotation on the
    corresponding property, which results in ACC_DEPRECATED in codegen.
    
     #KT-43326 Fixed

--- compiler/testData/codegen/bytecodeListing/deprecated/inlineClassTypesInSignature.kt
+++ compiler/testData/codegen/bytecodeListing/deprecated/inlineClassTypesInSignature.kt
@@ -7,3 +7,9 @@ abstract class A {
     @Deprecated("", level = DeprecationLevel.HIDDEN)
     abstract var hidden: UByte
 }
+
+interface I {
+    @Deprecated("")
+    val <T> T.id: T
+        get() = this
+}
